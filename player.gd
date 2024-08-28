2cac51f7937911b692dbcbee6e65d4377f58ad2a
John Carlo Lomocho <jhemueljohncarlo@gmail.com>
2024-08-28 13:13:49 +0800

    change camera movement

--- player.gd
+++ player.gd
@@ -6,12 +6,9 @@ var pitch_input := 0.0
 @onready var twist_pivot := $TwistPivot
 @onready var pitch_pivot := $TwistPivot/PitchPivot
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)  
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var input := Vector3.ZERO
 	input.x = Input.get_axis("move_left", "move_right")
@@ -21,18 +18,24 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
-	twist_pivot.rotate_y(twist_input)
-	pitch_pivot.rotate_x(pitch_input)
-	pitch_pivot.rotation.x = clamp(
-		$TwistPivot/PitchPivot.rotation.x,
-		deg_to_rad(-30),
-		deg_to_rad(30)
-	)
+	if twist_input != 0.0 or pitch_input != 0.0:
+		twist_pivot.rotate_y(twist_input)
+		pitch_pivot.rotate_x(pitch_input)
+		pitch_pivot.rotation.x = clamp(
+			$TwistPivot/PitchPivot.rotation.x,
+			deg_to_rad(-30),
+			deg_to_rad(30)
+		)
+	# Reset inputs
 	twist_input = 0.0
 	pitch_input = 0.0 
 	
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-			twist_input = - event.relative.x * mouse_sensitivity
-			pitch_input = - event.relative.y * mouse_sensitivity
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)  
+		if event is InputEventMouseMotion:
+			if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+				twist_input = -event.relative.x * mouse_sensitivity
+				pitch_input = -event.relative.y * mouse_sensitivity
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)  
